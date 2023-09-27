cd23f5bd2eb528d49a7ebaa1c1b2c32f58e968c4
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-27 08:02:33 +0000

    dataTypes, text_link

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS Code as newer version.
+
+// alert(3 + 3) // we are using nodejs, not browser.
+
+// code readability should be high.
+
+let bigNumber = BigInt(186465165465621656565656556498621654698562354987956126516849586135462132n);
+let Name = "Nitish"
+let lastName = 'Kumar'
+let age = 25
+let village;
+let country = null;
+
+console.table([bigNumber, Name, lastName, age, village, country]);
+
+/*
+number => 2 to power 53  || type -> 'number'
+bigNumber = BigInt(3564656165462321656231265621165161);  => for big number || type -> 'BigInt' 
+string => "",'' || type -> 'string'
+boolean => true/false || type -> 'boolean'
+null => standalone value, representation of empty value, or we can assign also to null value || type -> 'object'
+undefined => value not assigned || type -> 'undefined'
+symbol => unique,use to defined uniqueness || type -> ?????
+
+object
+
+*/ 
