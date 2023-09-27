165bb3a77b194920930a06f03f81ba16527c5858
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-27 10:54:52 +0000

    dataType sumary completed.

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive
+/*                                  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+          => For the above data types, their typeof is same as data type EXECEPT "null" -> 'object'.
+*/ 
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null  // null -> we can say that it is completely empty.
+// let userEmail;
+// const id = Symbol('123')
+// console.log(typeof id);
+// const anotherId = Symbol('123')
+// console.log(typeof anotherId);
+// console.log(id === anotherId);
+// const bigNumber = 3456543576654356754n
+// console.log(typeof bigNumber);
+
+
+
+// Reference (Non primitive)  : Note :-> For all non-primitive data types, Their return type is "object" only Except "function" -> function( Also called as function object)
+//                                      Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// console.log(typeof heros);
+
+// let myDetails = {
+//     name: "Nitish",
+//     age: 24,
+// }
+// console.log(typeof myDetails);
+
+// const myFunction = function(){
+//     console.log("Hello Nitish");
+// }
+// console.log(typeof myFunction);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
