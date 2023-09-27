dfd1b2c4399526fd69a1ca7776cd212a30041b89
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-27 11:49:57 +0000

    strings completed.

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+// const myName = "Nitish"
+// const repoCount = 7
+// // console.log(myName + repoCount + " Value");
+// console.log(`Hello Friends, My Name is ${myName} and my total repo count till now is ${repoCount}.`);
+
+const gameName = new String('Nitish_hc_com')
+
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);  /////////// we can use -ve value also.
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());   ///// to remove sapace at start and end.
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))   //// to replace any thing
+
+// console.log(url.includes('sundar'))    /// to check any thing in that string that they are present in that string or not
+
+console.log(gameName.split('_'));
