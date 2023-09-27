699170763dbc7a85fe1d2a445a96319113fcd282
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-27 08:39:20 +0000

    conversion completed.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let myName = "33cvg";
+
+// console.log(typeof myName);
+// console.log(typeof(myName));
+
+/////////// Number //////////////
+let valueInNumber = Number(myName); // here we try to convert value present in name into number using "Number()"
+console.log(typeof valueInNumber);  // here we try to check thier type
+console.log(valueInNumber);         // here we try to see the value present after changes
+
+/*
+            Notes :- 
+
+1. For Number :-
+=> "33" -> 33
+=> "33cvg" -> NaN
+=> true -> 1; false => 0;
+
+2. For Boolean :-
+=> 1 -> true; 0 -> false
+=> "" -> false
+=> "Nitish" -> true
+
+*/
