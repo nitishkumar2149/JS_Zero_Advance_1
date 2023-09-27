44b1bd378198269a99958ea123264a89045f1235
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-27 07:19:43 +0000

    variables completed.

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,14 @@
 const accountId = 445565;
+let accountEmail = "nitishkumar2149@gmail.com"
+var accountPassword = "Kg$4662381";
+accountCity = "Sitamrhi";
+let accountState;
+
+// accountId = 5 // 'canst' -> cann't be re-assigned.
+
+/*
+Prefer not to use 'var'
+because of issue in block scope and functional scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
