e68fc5a9e7b0d5581dfbb1408a8a048f8be299c2
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-27 09:07:40 +0000

    Operation completed.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+// ************************  Conversion  ******************
 let myName = "33cvg";
 
 // console.log(typeof myName);
@@ -5,8 +6,8 @@ let myName = "33cvg";
 
 /////////// Number //////////////
 let valueInNumber = Number(myName); // here we try to convert value present in name into number using "Number()"
-console.log(typeof valueInNumber);  // here we try to check thier type
-console.log(valueInNumber);         // here we try to see the value present after changes
+// console.log(typeof valueInNumber);  // here we try to check thier type
+// console.log(valueInNumber);         // here we try to see the value present after changes
 
 /*
             Notes :- 
@@ -22,3 +23,30 @@ console.log(valueInNumber);         // here we try to see the value present afte
 => "Nitish" -> true
 
 */
+
+
+// ************************  Operations  ******************
+
+// link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// go and check out the above link. 
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let a = 5;
+// let b = -a;
+// console.log(b);
+
+// console.log(2 + "2"); //22
+// console.log("2" + "2"); //22
+// console.log("2" + 2);  //22
+// console.log("2" + 3 + 1);  //231
+// console.log(2 + 3 + "1");  //51
+//console.log(2 + 3 + "1" + 5 + 9);  //5159
+
+// JavaScript convert the the whole number into string if string present in first place.
