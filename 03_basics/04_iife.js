17178a653e52c147b15e4f5d0c4462b73dea3963
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-29 07:38:17 +0000

    IIFE completed.

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+/* 
+    =>  Define IIFE -> IIFE stands for Immediately Invoked Function Expressions which is used to prevent the pollution from global scope variables.
+    => ()()   -> Here, inside 1st (), we use it for decleration and 2nd () is used for execuation.
+
+*/
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('NitIsh')
