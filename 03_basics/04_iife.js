d1bffdb6bb3f84ab67abc3bafa0003315e5a95c6
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-09-29 07:55:48 +0000

    ";" -> is required at end of iife i.e -> ()();

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,6 @@
 /* 
     =>  Define IIFE -> IIFE stands for Immediately Invoked Function Expressions which is used to prevent the pollution from global scope variables.
-    => ()()   -> Here, inside 1st (), we use it for decleration and 2nd () is used for execuation.
+    => ()();   -> Here, inside 1st (), we use it for decleration and 2nd () is used for execuation.
 
 */
 
@@ -12,4 +12,4 @@
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('NitIsh')
+} )('Nitish');
