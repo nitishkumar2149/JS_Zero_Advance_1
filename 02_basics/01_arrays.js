58f99fd314935e63b1cf3937c39f3c7c310347a7
Nitish Kumar <81761846+nitishkumar2149@users.noreply.github.com>
2023-10-01 04:56:13 +0000

    array method updated.

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,33 +6,33 @@
             => In JS, array value is access by thier index and index start from 0,1,2,3,....
             => In JS, array-copy operation create shallow copies( A shallow copy of an object is a copy whose properties share the same references. ) not deep copy( A deep copy of an object is a copy whose properties do not share the same references. ).
 */
-// let multipleElements = [1, 2, 5, 'n', "h", "Nitish", null, undefined, true, 0, false, [1, 8, "h", "jhdvc"]];
-// let value = multipleElements[11];
+let multipleElements = [1, 2, 5, 'n', "h", "Nitish", null, undefined, true, 0, false, [1, 8, "h", "jhdvc"]];
+let value = multipleElements[11];
 // console.log(multipleElements[11]);
 // console.log(typeof value);
 // console.log(multipleElements.length)
 
-// const myArr = [0, 1, 2, 3, 4, 5]
-// const myHeors = ["shaktiman", "naagraj"]
+const myArr = [0, 1, 2, 3, 9, 5]
+const myHeors = ["shaktiman", "naagraj"]
 
-// const myArr2 = new Array(1, 2, 3, 4)
+const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
 // Array methods
 
-// myArr.push(6)  // .push() -> push function will add that value in an array.
+// myArr.push(6)  // .push() -> push method will add that value in an array.
 // myArr.push(7)
-// myArr.pop()    // .pop() -> pop function will remove the last value from that array.
+// myArr.pop()    // .pop() -> pop method will remove the last value from that array.
 
-// myArr.unshift(9)  // .unshift() -> unshift function add the value at '0' index in that array and due to this in backend all the previous value present in that array shifted forward, this maker the "system slow". So, This is "not good approch".
-// myArr.shift()     // .shift()  -> shift function remove the value that present at '0' index.
+// myArr.unshift(9)  // .unshift() -> unshift method add the value at '0' index in that array and due to this in backend all the previous value present in that array shifted forward, this maker the "system slow". So, This is "not good approch".
+// myArr.shift()     // .shift()  -> shift method remove the value that present at '0' index.
 
 
 // console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.indexOf(9));
 
 
-// const newArr = myArr.join()  // .join() -> join function convert the array object into string.
+const newArr = myArr.join()  // .join() -> join method convert the array object into string.
 // console.log(myArr);
 // console.log(typeof myArr);
 // console.log(newArr);
@@ -41,15 +41,19 @@
 
 // ************************* slice, splice *************************
 
-// const myOriginalArr = [10, 1, 2, 3, 4, 5]
+const myOriginalArr = [10, 1, 2, 3, 4, 5]
 // console.log("A ", myOriginalArr);  //Expcted o/p -> A [10, 1, 2, 3, 4, 5]
 
-// const myFirstArr_A = myOriginalArr.slice(1, 3)  // here .slice(n1, n2)  -> Slice function will return value from n1 index to (n2-1) index and it will not affect the original value.
+const myFirstArr_A = myOriginalArr.slice(1, 3)  // here .slice(n1, n2)  -> Slice method will return value from n1 index to (n2-1) index and it will not affect the original value.
 // console.log(myFirstArr_A);  //Expcted o/p -> [1, 2]
 
 // console.log("B ", myOriginalArr);   //Expcted o/p -> B [10, 1, 2, 3, 4, 5]
-// const mySecondArr_B = myOriginalArr.splice(1, 3)   //Here .splice(n1, n2) -> Splice function will return value from (n1 to n2) index and it will also remove those value present in original array from (n1 to n2) index. 
+const mySecondArr_B = myOriginalArr.splice(1, 3)   //Here .splice(n1, n2) -> Splice method will return value from (n1 to n2) index and it will also remove those value present in original array from (n1 to n2) index. 
 // console.log(myFirstArr_A);  //Expcted o/p -> [1, 2]
 // console.log(mySecondArr_B);   //Expcted o/p -> [1, 2, 3] 
 
 // console.log("C ", myOriginalArr);   //Expcted o/p -> C [10, 4, 5]
+
+
+
+
--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -6,18 +6,26 @@ marvel_heros.push(dc_heros)  // here .push() methods can  push anything in an ar
 // console.log("**************************************");
 
 const marvel_heros_1 = ["thor", "Ironman", "spiderman"]
-const dc_heros_1 = ["superman", "flash", "batman"]
+const dc_heros_1 = ["superman_1", "flash_1", "batman_1"]
+const dc_heros_2 = ["superman_2", "flash_2", "batman_2"]
+const dc_heros_3 = ["superman_3", "flash_3", "batman_3"]
+
+const all_heros_value = dc_heros.concat(dc_heros_1.concat(dc_heros_2.concat(dc_heros_3)));
+// console.log(all_heros_value);  // return all value in a single array
+// console.log(typeof all_heros_value);  // object
+// console.log(...dc_heros,...dc_heros_1,...dc_heros_2,...dc_heros_3);
+
+
 const allHeros = marvel_heros_1.concat(dc_heros_1)
 // console.log(allHeros);
 // console.log(allHeros[3][1]);
 // console.log("**************************************");
 
 /*                 Note :- 
-.push() method directly push the value as it in present like if they are present in array then they push all the value in a single array in another array object.
-.concat() methos also push all the value in an array but individually in a new array object. 
-[...arrayName_1, ...arrayName_2, ...arrayName_3] -> also known as spread operator which spread all the values present in multiple arrays but not spread the values present array inside in an array inside an array.
-
-.flat(depth) method spread all values which is not done by spread operator.
+-> .push() method directly push the value as it in present like if they are present in array then they push the whole array like a single value in another array and return an array inside an array like array object.
+-> .concat() methos also push all the value in an array but individually in a new array object. 
+-> [...arrayName_1, ...arrayName_2, ...arrayName_3] -> also known as spread operator which spread all the values present in multiple arrays but not spread the values present array inside in an array inside an array.
+-> .flat(depth) method spread all values which is not done by spread operator.  Here "depth" -> means how many penitration i.e array inside array inside array need to be done.
 
 */
 
@@ -36,18 +44,20 @@ const all_values = [...marvel_heros, ...dc_heros, ...another_array]
 
 
 const real_another_array = another_array.flat(Infinity)  // .flat(depth) method spread all values.
+
 // console.log(real_another_array);
-
-
-
 // console.log(Array.isArray("Hitesh"))  // Array.isArray("value") method will tell me that the "value" that i am asking is an array or not.
+
 const converted_Array = Array.from("Hitesh"); //  Array.from("value") method, convert that value into an array.
 // console.log(converted_Array);  
 // console.log(Array.isArray(converted_Array));
 // console.log(Array.from({name: "hitesh"})) // interesting  => it gives empty array because we need to tell them that how they will convert them into an array by using "key" or by using "value".
 
 let score1 = 100
-let score2 = 200
-let score3 = 300
+const score2 = 200
+var score3 = 300
+const arr = [1,4,5]
+let obj = { key_1: "values_1",key_2: "values_2",key_3: "values_3"}
 
-// console.log(Array.of(score1, score2, score3));  // here Array.of(value) method also convert values into a new array. Here value -> A set of elements which may be a variables, arrays, ..etc.  
+// console.log(Array.of(score1, score2, score3, arr, obj));  // Array.of(values) method store and return all values into a new array. Here 'values' -> A set of elements which may be a variables_value, arrays_value, obaject_value...etc. 
+
